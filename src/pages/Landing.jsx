1eb850e556d4b50557a0cff0399df09304534eb2
Rahaf-Ali22 <rahafu722@gmail.com>
2025-12-29 21:03:31 +0200

    update landing page styles and animation

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -8,59 +8,48 @@ function Landing() {
 
   return (
     <div className="landing">
-      {/* NAVBAR */}
       <header className="navbar">
         <div className="nav-left">
-          <button
-            className="btn-outline"
-            onClick={() => navigate("/signup")}
-          >
+          <button className="btn-outline" onClick={() => navigate("/home")}>
             Get Started
           </button>
 
-          <button
-            className="btn-login"
-            onClick={() => navigate("/login")}
-          >
+          <button className="btn-login" onClick={() => navigate("/login")}>
             Login
           </button>
         </div>
 
         <div className="nav-right">
-          <span
-            className="nav-link"
-            onClick={() => setShowAbout(true)}
-          >
+          <span className="nav-link" onClick={() => setShowAbout(true)}>
             About Us
           </span>
         </div>
       </header>
 
-      {/* HERO */}
       <main className="hero">
-        <h1>COGNITO</h1>
+        <div className="hero-title-wrapper">
+          <h1 className="hero-title">
+          <span data-text="COGNITO">COGNITO</span>
+          </h1>
+
+        </div>
+
         <p>
           A smart platform to assess your IT level, choose a field,
           and progress through a structured learning roadmap.
         </p>
       </main>
 
-      {/* ABOUT MODAL */}
       {showAbout && (
         <div className="modal-overlay">
           <div className="modal">
             <h2>About Cognito</h2>
             <p>
-              Cognito is a smart IT learning platform designed to help
-              students assess their skill level, choose the appropriate
-              learning path, and track their progress through structured
-              roadmaps, quizzes, and performance analysis.
+              Cognito helps students assess skills, choose learning paths,
+              and track progress through structured IT roadmaps.
             </p>
 
-            <button
-              className="close-btn"
-              onClick={() => setShowAbout(false)}
-            >
+            <button className="close-btn" onClick={() => setShowAbout(false)}>
               Close
             </button>
           </div>
